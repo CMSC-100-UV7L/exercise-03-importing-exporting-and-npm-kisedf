07f4080eae6d789986b778adeb2804386df60b23
kisedf <clflauta@up.edu.ph>
2023-03-16 22:37:11 +0800

    Docs: added documentation for generateUniqueID function

--- functions.js
+++ functions.js
@@ -1,9 +1,17 @@
+// Chesedh Flauta
+// UV 7L
+// March 16, 2023
+
 import { v4 as uuidv4 } from 'uuid'
 
-function generateUniqueID(firstName, lastName) {
-    return firstName[0].toLowerCase() 
-            + lastName.toLowerCase() 
-            + uuidv4().substring(0, 8)
+function generateUniqueID(firstName, lastName) {    // Input parameters: first name (string), last name (string)
+    return firstName[0].toLowerCase()               // first letter of the first name (converted to lowercase)
+            + lastName.toLowerCase()                // last name (converted to lowercase)
+            + uuidv4().substring(0, 8)              // “unique” alphanumeric string of length 8
+}
+
+function addAccount(accountArr) { 
+
 }
 
 console.log(generateUniqueID("Alan", "Turing"))
